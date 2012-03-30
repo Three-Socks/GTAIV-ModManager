13c5174502709529b8061d7c3795942d0aa9b712
Three-Socks <mark@playstationgl.com>
2012-03-30 20:50:59 +0100

    1.1
    --
    user: Three-Socks
    changed modmanager.c

--- modmanager.c
+++ modmanager.c
@@ -1,11 +1,11 @@
 /**
- * GTAIV ModManager V1
+ * GTAIV ModManager V1.1
  *
  * This file is from: https://bitbucket.org/ThreeSocks/gtaiv-modmanager/
  *
  * By Three-Socks
  *
- * Important: Make sure you replace the natives.h with your compiler includes. The one inside the /inc folder. It contains a fix that is needed for modmanager to compile correctly.
+ * Important: Make sure you replace the natives.h with your compiler includes. The one inside the /inc folder. It contains a bug fix that is needed for modmanager to compile correctly.
  *
  */
 
@@ -35,38 +35,48 @@ script mod[21];
 int item_highlighted = 1;
 int mod_count = 0; 
 float edc_width = 0;
-float Ipos_y, title_y, title_width, title_height;
+float Ipos_y, start_pos_y, title_y, title_width, title_height;
 uint title_r = 253, title_g = 160, title_b = 35;
 
-float pos_x = 0.16500000, width = 0.40000000, height = 0.40000000;
-uint r, g, b, a = 255;
+float pos_x = 0.1650, width = 0.4000, height = 0.4000;
+uint r, g, b, a;
 
 void Init(void)
 {
 
 	// Script config
-	// Add you scripts to these three arrays (file, name, enabled (always = 0)) and then increment the mod_count if needed.
+	// Add you scripts to these two arrays (file, name) and then increment the mod_count if needed.
 
 	// The file name of the script
-	mod[1].file = "brucie_garage";
+	mod[1].file = "garage";
 	mod[2].file = "xyzh_coords_example";
 	mod[3].file = "button_input_example";
-	mod[4].file = "teleport_example";
-	mod[5].file = "";
+	mod[4].file = "open_garages_example";
+	mod[5].file = "vehicle_example";
+	mod[6].file = "disable_airportpol";
+	mod[7].file = "skylift_pickup";
+	mod[8].file = "teleport_example";
+	mod[9].file = "teleport_scrapyard";
+	mod[10].file = "";
 
 	// The name to display
-	mod[1].name = "Brucies Garage Shop Mod";
+	mod[1].name = "Garage Shop Mod";
 	mod[2].name = "XYZH Coords Example";
 	mod[3].name = "Button Input Example";
-	mod[4].name = "Teleport to Brucie's Garage";
-	mod[5].name = "";
+	mod[4].name = "Open Garages Example";
+	mod[5].name = "Vehicle Example";
+	mod[6].name = "Disable Police Patrol at Airport";
+	mod[7].name = "Skylift Pick Up";
+	mod[8].name = "Teleport to Brucies Garage";
+	mod[9].name = "Teleport to Northwood Scrap Yard";
+	mod[10].name = "";
 
 	// How many scirpts there are.
-	mod_count = 4;
+	mod_count = 9;
 	
 	// End script config
 	// No more editing needed below this point if all your doing is adding scripts...
-	
+
 	int I;
 	for (I = 1; I < (mod_count + 1); I++)
 	{
@@ -81,52 +91,81 @@ void Init(void)
 
 	if (GET_IS_WIDESCREEN())
 	{
-		title_y = 0.21200000;
+		title_y = 0.2120;
 		title_width = 0.77999990;
-		title_height = 1.20000000;
+		title_height = 1.2000;
 	}
 	else
 	{
-		title_y = 0.21800000;
-		title_width = 0.60000000;
-		title_height = 0.98300000;
+		title_y = 0.2180;
+		title_width = 0.6000;
+		title_height = 0.9830;
 	}
+
+	start_pos_y = title_y + 0.1300;
 }
 
 void DrawMenu(void)
 {
+	Ipos_y = start_pos_y;
+
 	if (IS_BUTTON_JUST_PRESSED(0, BUTTON_DPAD_DOWN))
-	{
+	{	
+
 		if (item_highlighted == mod_count)
 		{
 			item_highlighted = 1;
+			start_pos_y = title_y + 0.1300;
 		}
 		else
 		{
 			item_highlighted++;
 		}
+		
+		if (item_highlighted >= 13)
+		{
+			start_pos_y -= 0.0400;
+		}
+
 	}
 
 	if (IS_BUTTON_JUST_PRESSED(0, BUTTON_DPAD_UP))
 	{
+	
 		if (item_highlighted == 1)
 		{
 			item_highlighted = mod_count;
+
+			if (mod_count >= 13)
+			{
+				start_pos_y = (title_y + 0.1300) - ((mod_count - 12) * 0.0400);
+			}
 		}
 		else
 		{
 			item_highlighted--;
 		}
+
+		if (item_highlighted >= 13)
+		{
+			start_pos_y += 0.0400;
+		}
+
 	}
 
 	int I;
-	Ipos_y = title_y + 0.13000000;
 	for (I = 1; I < (mod_count + 1); I++)
 	{
 		r = 255;
 		g = 255;
 		b = 255;
-		Ipos_y = Ipos_y + 0.04000000;
+		a = 255;
+		Ipos_y = Ipos_y + 0.0400;
+
+		if (Ipos_y < title_y + 0.1300)
+		{
+			a = 0;
+		}
 
 		if (item_highlighted == I)
 		{
@@ -160,12 +199,12 @@ void DrawMenu(void)
 			if (mod[I].enabled == 2)
 			{
 				set_up_draw(0, width, height, 253, 160, 35, a);
-				draw_text("STRING", pos_x + edc_width + 0.09000000, Ipos_y, "Enabled");
+				draw_text("STRING", pos_x + edc_width + 0.0900, Ipos_y, "Enabled");
 			}
 			else
 			{
 				set_up_draw(0, width, height, 255, 255, 255, a);
-				draw_text("STRING", pos_x + edc_width + 0.09000000, Ipos_y, "Disabled");
+				draw_text("STRING", pos_x + edc_width + 0.0900, Ipos_y, "Disabled");
 			}
 		}
 
@@ -180,13 +219,14 @@ void DoMenu(void)
 	}
 	PAUSE_GAME();
 	HIDE_HUD_AND_RADAR_THIS_FRAME();
-	DRAW_RECT(0.00000000, 0.00000000, 2.00000000, 2.00000000, 0, 0, 0, 255);
+	DRAW_RECT(0.5000, 0.5000, 1.0000, 1.0000, 0, 0, 0, 255);
 	INIT_FRONTEND_HELPER_TEXT();
 	DRAW_FRONTEND_HELPER_TEXT("LEAVE", "INPUT_F_CANCEL", 0);
 	DRAW_FRONTEND_HELPER_TEXT("CONFIRM", "INPUT_F_ACCEPT", 0);
 
-	draw_title(0.50000000, title_y, title_width, title_height, title_r, title_g, title_b, 255, "SCO ModManager");
 	DrawMenu();
+	draw_title(0.5000, title_y, title_width, title_height, title_r, title_g, title_b, 255, "SCO ModManager");
+
 	if (IS_BUTTON_JUST_PRESSED(0, BUTTON_X))
 	{
 		if (mod[item_highlighted].enabled == 2)
@@ -204,9 +244,15 @@ void DoMenu(void)
 			}
 			START_NEW_SCRIPT(mod[item_highlighted].file, 1024);
 			MARK_SCRIPT_AS_NO_LONGER_NEEDED(mod[item_highlighted].file);
-			UNPAUSE_GAME();
-			SET_PLAYER_CONTROL(GetPlayerIndex(), true);
-			TERMINATE_THIS_SCRIPT();
+			
+			int I;
+			for (I = 1; I < (mod_count + 1); I++)
+			{
+				if ((GET_NUMBER_OF_INSTANCES_OF_STREAMED_SCRIPT(mod[I].file)) >= 1)
+				{
+					mod[I].enabled = 2;
+				}
+			}
 		}
 		else
 		{
@@ -230,6 +276,12 @@ void main(void)
 	while(true)
 	{
 		WAIT(0);
+		if (IS_SCREEN_FADED_OUT())
+		{
+			UNPAUSE_GAME();
+			SET_PLAYER_CONTROL(GetPlayerIndex(), true);
+			TERMINATE_THIS_SCRIPT();
+		}
 		DoMenu();
 	}
 }
